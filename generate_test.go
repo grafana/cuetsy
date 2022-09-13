835f153f8085697865b25aa27e85c0d67e601081
sam boyer <sdboyer@grafana.com>
2022-09-13 10:16:02 -0400

    tests: Add skipped case for single embeds (#60)

--- generate_test.go
+++ generate_test.go
@@ -46,6 +46,7 @@ func TestGenerateWithImports(t *testing.T) {
 		ToDo: map[string]string{
 			"imports/oneref_verbose":   "Figure out how to disambiguate struct literals from the struct-with-braces-and-one-element case",
 			"imports/struct_shorthand": "Shorthand struct notation is currently unsupported, needs fixing",
+			"imports/single_embed":     "Single-item struct embeds should be treated as just another interface to compose, but get confused with references - #60",
 		},
 	}
 
